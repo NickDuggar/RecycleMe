21785d540c4bf7fd7cc6101abd81d9bb36e4f8f0
NickDuggar <nbduggar@gmail.com>
2018-10-21 02:43:23 -0500

    Reformatted IdentifyActivity

--- app/src/main/java/recycleme/nickduggar/com/recycleme/IdentifyActivity.java
+++ app/src/main/java/recycleme/nickduggar/com/recycleme/IdentifyActivity.java
@@ -28,108 +28,102 @@ import java.util.Map;
 public class IdentifyActivity extends AppCompatActivity {
 
     ImageView image;
-    Button choose, upload;
+    Button another, upload;
     int PICK_IMAGE_REQUEST = 111;
     String URL ="http://192.168.1.101/JavaRESTfullWS/DemoService/upload";
     Bitmap bitmap;
     ProgressDialog progressDialog;
+    RequestQueue rQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        image = (ImageView)findViewById(R.id.image);
-        choose = (Button)findViewById(R.id.choose);
-        upload = (Button)findViewById(R.id.upload);
-
-        //opening image chooser option
-        choose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_PICK);
-                startActivityForResult(Intent.createChooser(intent, "Select Image"), PICK_IMAGE_REQUEST);
-            }
-        });
+        image = findViewById(R.id.image);
+        another = findViewById(R.id.button_another);
+        upload = findViewById(R.id.button_identify);
 
         upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                progressDialog = new ProgressDialog(MainActivity.this);
-                progressDialog.setMessage("Uploading, please wait...");
-                progressDialog.show();
 
-                //converting image to base64 string
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-                byte[] imageBytes = baos.toByteArray();
-                final String imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT);
-
-                //sending image to server
-                StringRequest request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>(){
-                    @Override
-                    public void onResponse(String s) {
-                        progressDialog.dismiss();
-                        if(s.equals("true")){
-                            Toast.makeText(MainActivity.this, "Uploaded Successful", Toast.LENGTH_LONG).show();
-                        }
-                        else{
-                            Toast.makeText(MainActivity.this, "Some error occurred!", Toast.LENGTH_LONG).show();
-                        }
-                    }
-                },new Response.ErrorListener(){
-                    @Override
-                    public void onErrorResponse(VolleyError volleyError) {
-                        Toast.makeText(MainActivity.this, "Some error occurred -> "+volleyError, Toast.LENGTH_LONG).show();;
-                    }
-                }) {
-                    //adding parameters to send
-                    @Override
-                    protected Map<String, String> getParams() throws AuthFailureError {
-                        Map<String, String> parameters = new HashMap<String, String>();
-                        parameters.put("image", imageString);
-                        return parameters;
-                    }
-                };
-
-                RequestQueue rQueue = Volley.newRequestQueue(MainActivity.this);
-                rQueue.add(request);
             }
         });
     }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_identify);
 
-
-
+    public void anotherButtonClicked(View view) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_PICK);
+        startActivityForResult(Intent.createChooser(intent, "Select Image"), PICK_IMAGE_REQUEST);
     }
 
-    final TextView mTextView = (TextView) findViewById(R.id.text);
-    // ...
+    public void identifyButtonPressed(View view) {
+        progressDialog = new ProgressDialog(IdentifyActivity.this);
+        progressDialog.setMessage("Uploading, please wait...");
+        progressDialog.show();
 
-    // Instantiate the RequestQueue.
-    RequestQueue queue = Volley.newRequestQueue(this);
-    String url ="http://www.google.com";
+        //converting image to base64 string
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        final String imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT);
 
-    // Request a string response from the provided URL.
-    StringRequest stringRequest = new StringRequest(DownloadManager.Request.Method.GET, url,
-            new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    // Display the first 500 characters of the response string.
-                    mTextView.setText("Response is: "+ response.substring(0,500));
+        //sending image to server
+        StringRequest request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>(){
+            @Override
+            public void onResponse(String s) {
+                progressDialog.dismiss();
+                if(s.equals("true")){
+                    Toast.makeText(IdentifyActivity.this, "Uploaded Successful", Toast.LENGTH_LONG).show();
                 }
-            }, new Response.ErrorListener() {
-        @Override
-        public void onErrorResponse(VolleyError error) {
-            mTextView.setText("That didn't work!");
-        }
-    });
+                else{
+                    Toast.makeText(IdentifyActivity.this, "Some error occurred!", Toast.LENGTH_LONG).show();
+                }
+            }
+        },new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Toast.makeText(IdentifyActivity.this, "Some error occurred -> "+volleyError, Toast.LENGTH_LONG).show();;
+            }
+        }) {
+            //adding parameters to send
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parameters = new HashMap<String, String>();
+                parameters.put("image", imageString);
+                return parameters;
+            }
+        };
 
-    // Add the request to the RequestQueue.
-    queue.add(stringRequest);
+        rQueue = Volley.newRequestQueue(IdentifyActivity.this);
+        rQueue.add(request);
+    }
+
+
+
+//    final TextView mTextView = (TextView) findViewById(R.id.text);
+//
+//    // Instantiate the RequestQueue.
+//    RequestQueue queue = Volley.newRequestQueue(this);
+//    String url ="http://www.google.com";
+//
+//    // Request a string response from the provided URL.
+//    StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//            new Response.Listener<String>() {
+//                @Override
+//                public void onResponse(String response) {
+//                    // Display the first 500 characters of the response string.
+//                    mTextView.setText("Response is: "+ response.substring(0,500));
+//                }
+//            }, new Response.ErrorListener() {
+//        @Override
+//        public void onErrorResponse(VolleyError error) {
+//            mTextView.setText("That didn't work!");
+//        }
+//    });
+//
+//    // Add the request to the RequestQueue.
+//    queue.add(stringRequest);
 }
